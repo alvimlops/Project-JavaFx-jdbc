075e8847edf1edde98748e0f9587d0b4591acc05
alvaro rodrigues <alvim.lopez@gmail.com>
2021-10-09 15:39:58 -0300

    criando o About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,11 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -20,7 +25,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -29,6 +34,11 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
